fe06d74160b08f1456784cc70614a3525a190e84
Juwan <junhuanchen@qq.com>
2019-07-20 21:35:33 +0800

    Update device.hpp

--- device.hpp
+++ device.hpp
@@ -29,20 +29,20 @@ struct Wearfit
 {
     static void printReport(const ble_gap_evt_adv_report_t *report)
     {
-        Serial.print("  rssi: ");
-        Serial.println(report->rssi);
-        Serial.print("  scan_rsp: ");
-        Serial.println(report->type.scan_response);
-        // Serial.print("  type: ");
-        // Serial.println(report->type);
-        Serial.print("  dlen: ");
-        Serial.println(report->data.len);
-        Serial.print("  data: ");
+        // Serial.print("  rssi: ");
+        // Serial.println(report->rssi);
+        // Serial.print("  scan_rsp: ");
+        // Serial.println(report->type.scan_response);
+        // // Serial.print("  type: ");
+        // // Serial.println(report->type);
+        // Serial.print("  dlen: ");
+        // Serial.println(report->data.len);
+        // Serial.print("  data: ");
         for (int i = 0; i < report->data.len; i += sizeof(uint8_t))
         {
-            Serial.printf("%02X-", report->data.p_data[i]);
+            // Serial.printf("%02X-", report->data.p_data[i]);
         }
-        Serial.println("");
+        // Serial.println("");
     }
 
     static void printHexList(uint8_t *buffer, uint8_t len)
@@ -50,9 +50,9 @@ struct Wearfit
         // print forward order
         for (int i = 0; i < len; i++)
         {
-            Serial.printf("%02X-", buffer[i]);
+            // Serial.printf("%02X-", buffer[i]);
         }
-        Serial.println();
+        // Serial.println();
     }
 
     static void disconnect_callback(uint16_t conn_handle, uint8_t reason)
@@ -60,71 +60,72 @@ struct Wearfit
         (void)conn_handle;
         (void)reason;
         WearfitAlive = false;
-        Serial.println("Disconnected");
+        // Serial.println("Disconnected");
     }
 
     static void connect_callback(uint16_t conn_handle)
     {
-        Serial.println("");
-        Serial.print("Connect Callback, conn_handle: ");
-        Serial.println(conn_handle);
+        // Serial.println("");
+        // Serial.print("Connect Callback, conn_handle: ");
+        // Serial.println(conn_handle);
 
         /* Complete Local Name */
         uint8_t buffer[BLE_GAP_ADV_SET_DATA_SIZE_MAX] = {0};
 
         // If Service is not found, disconnect and return
-        Serial.print("Discovering Wearfit Service ... ");
+        // Serial.print("Discovering Wearfit Service ... ");
         if (!WearfitService.discover(conn_handle))
         {
-            Serial.println("No Service Found");
+            // Serial.println("No Service Found");
 
             // disconect since we couldn't find service
             Bluefruit.disconnect(conn_handle);
 
             return;
         }
-        Serial.println("Service Found");
+        // Serial.println("Service Found");
 
         // Once service is found, we continue to discover the primary characteristic
-        Serial.print("Discovering WearfitNotify Characteristic ... ");
+        // Serial.print("Discovering WearfitNotify Characteristic ... ");
         if (!WearfitNotify.discover())
         {
             // Measurement chr is mandatory, if it is not found (valid), then disconnect
-            Serial.println("No Characteristic Found. Characteristic is mandatory but not found. ");
+            // Serial.println("No Characteristic Found. Characteristic is mandatory but not found. ");
             Bluefruit.disconnect(conn_handle);
             return;
         }
-        Serial.println("Characteristic Found");
+        // Serial.println("Characteristic Found");
 
-        Serial.print("Enabling Notify on the Characteristic ... ");
+        // Serial.print("Enabling Notify on the Characteristic ... ");
         if (WearfitNotify.enableNotify())
         {
-            Serial.println("enableNotify success, now ready to receive Characteristic values");
+            // Serial.println("enableNotify success, now ready to receive Characteristic values");
         }
         else
         {
-            Serial.println("Couldn't enable notify for Characteristic. Increase DEBUG LEVEL for troubleshooting");
+            // Serial.println("Couldn't enable notify for Characteristic. Increase DEBUG LEVEL for troubleshooting");
         }
 
-        Serial.print("Discovering WearfitWrite Characteristic ... ");
+        // Serial.print("Discovering WearfitWrite Characteristic ... ");
         if (!WearfitWrite.discover())
         {
-            Serial.println("No Characteristic Found. Characteristic is mandatory but not found.");
+            // Serial.println("No Characteristic Found. Characteristic is mandatory but not found.");
             Bluefruit.disconnect(conn_handle);
             return;
         }
         else
         {
-            set_ring_shake();
+            // set_ring_shake();
+            set_notice("连接成功");
             WearfitAlive = true;
         }
-        Serial.println("Characteristic Found");
+        // Serial.println("Characteristic Found");
     }
 
     static void scan_callback(ble_gap_evt_adv_report_t *report)
     {
-        Serial.println("");
-        Serial.println("Scan Callback");
+        // Serial.println("");
+        // Serial.println("Scan Callback");
         printReport(report);
 
         uint8_t buffer[BLE_GAP_ADV_SET_DATA_SIZE_MAX] = {0};
@@ -132,84 +133,184 @@ struct Wearfit
         /* Display the timestamp and device address */
         if (report->type.scan_response)
         {
-            Serial.printf("[SR%10d] Packet received from ", millis());
+            // Serial.printf("[SR%10d] Packet received from ", millis());
         }
         else
         {
-            Serial.printf("[ADV%9d] Packet received from ", millis());
+            // Serial.printf("[ADV%9d] Packet received from ", millis());
         }
         // MAC is in little endian --> print reverse
-        Serial.printBufferReverse(report->peer_addr.addr, 6, ':');
-        Serial.print("\n");
+        // Serial.printBufferReverse(report->peer_addr.addr, 6, ':');
+        // Serial.print("\n");
 
         /* Raw buffer contents */
-        Serial.printf("%14s %d bytes\n", "PAYLOAD", report->data.len);
+        // Serial.printf("%14s %d bytes\n", "PAYLOAD", report->data.len);
         if (report->data.len)
         {
-            Serial.printf("%15s", " ");
-            Serial.printBuffer(report->data.p_data, report->data.len, '-');
-            Serial.println();
+            // Serial.printf("%15s", " ");
+            // Serial.printBuffer(report->data.p_data, report->data.len, '-');
+            // Serial.println();
         }
 
         /* RSSI value */
-        Serial.printf("%14s %d dBm\n", "RSSI", report->rssi);
+        // Serial.printf("%14s %d dBm\n", "RSSI", report->rssi);
 
         /* Adv Type */
-        Serial.printf("%14s ", "ADV TYPE");
+        // Serial.printf("%14s ", "ADV TYPE");
         if (report->type.connectable)
         {
-            Serial.print("Connectable ");
+            // Serial.print("Connectable ");
         }
         else
         {
-            Serial.print("Non-connectable ");
+            // Serial.print("Non-connectable ");
         }
 
         if (report->type.directed)
         {
-            Serial.println("directed");
+            // Serial.println("directed");
         }
         else
         {
-            Serial.println("undirected");
+            // Serial.println("undirected");
         }
 
         // 此处做设备地址的绑定和判断 4B:FC:1B:0C:77:99 // F6:37:07:D8:2A:F8
-        Serial.printBuffer(report->peer_addr.addr, 6, '-'), Serial.println(" mac\n");
-        if (0 == memcmp((const char *)report->peer_addr.addr, "\xF8\x2A\xD8\x07\x37\xF6", 6)) // F8-2A-D8-07-37-F6
+        // Serial.printBuffer(report->peer_addr.addr, 6, '-'), Serial.println(" mac\n");
+        if (true && 0 == memcmp((const char *)report->peer_addr.addr, "\xF8\x2A\xD8\x07\x37\xF6", 6)) // MAC F8-2A-D8-07-37-F6
         {
-            Serial.print("Parsing report for Local Name ... ");
+            // Serial.print("Parsing report for Local Name ... ");
             if (Bluefruit.Scanner.parseReportByType(report, BLE_GAP_AD_TYPE_COMPLETE_LOCAL_NAME, buffer, sizeof(buffer)))
             {
-                Serial.println("Found Local Name");
-                Serial.printf("%14s %s\n", "Local Name:", buffer);
+                // Serial.println("Found Local Name");
+                // Serial.printf("%14s %s\n", "Local Name:", buffer);
 
-                Serial.print("   Local Name data: ");
+                // Serial.print("   Local Name data: ");
                 printHexList(buffer, BLE_GAP_ADV_SET_DATA_SIZE_MAX);
 
-                Serial.print("Determining Local Name Match ... ");
+                // Serial.print("Determining Local Name Match ... ");
                 if (!memcmp(buffer, ADV_COMPLETE_LOCAL_NAME, sizeof(ADV_COMPLETE_LOCAL_NAME)))
                 {
-                    Serial.println("Local Name Match!");
+                    // Serial.println("Local Name Match!");
 
+                    Serial.printBuffer(report->peer_addr.addr, 6, '-'), Serial.println(" Mac\n");
                     Serial.println("Connecting to Peripheral ... ");
                     Bluefruit.Central.connect(report);
                 }
                 else
                 {
-                    Serial.println("No Match");
+                    // Serial.println("No Match");
                     Bluefruit.Scanner.resume(); // continue scanning
                 }
             }
         }
         else
         {
-            Serial.println("Failed");
+            // Serial.println("Failed");
 
             // For Softdevice v6: after received a report, scanner will be paused
             // We need to call Scanner resume() to continue scanning
             Bluefruit.Scanner.resume();
         }
+    }
+
+    static void notify_callback(BLEClientCharacteristic *chr, uint8_t *data, uint16_t len)
+    {
+        // Serial.print("Optical data: ");
+        // // Serial.printf("Optical len %d data: %.*s\n", len, len, data);
+        printHexList(data, len);
+
+        recv_forward(data, len);
+    }
+
+    static void recv_forward(uint8_t *buffer, uint8_t len)
+    {
+        if (buffer[4] == 0x31)
+        {
+            if (buffer[5] == 0x0A)
+            {
+                Serial.printf("H:%02X\n", buffer[6]); // 心率
+            }
+        }
+        if (buffer[4] == 0x91)
+        {
+            Serial.printf("B:%02X\n", buffer[7]); // 电量
+        }
+    }
+
+    static void get_heart_rate()
+    {
+        WearfitWrite.write("\xAB\x00\x04\xFF\x31\x0A\x01", 7);
+        // Serial.print("get_heart_rate");
+    }
+
+    // static void get_battery()
+    // {
+    //     WearfitWrite.write("\xAB\x00\x03\xFF\xB2\x80", 6);
+    //     // Serial.print("get_battery");
+    // }
+
+    static void set_ring_shake()
+    {
+        WearfitWrite.write("\xAB\x00\x04\xFF\xB1\x80\x01", 7);
+        // Serial.print("set_ring_shake");
+    }
+
+    static void set_notice(char *info)
+    {
+        char buf[30] = "\xAB\x00\x05\xFF\x72\x80\x03\x02"; // 8
+        // WearfitWrite.write("\xAB\x00\x05\xFF\x72\x80\x09\x01", 8); // 0
+        sprintf(buf + 8, "%s", info);
+        buf[2] = buf[2] + strlen(info);
+        WearfitWrite.write(buf, 8 + strlen(info)); // 0
+        // printHexList((uint8_t *)buf, 8 + strlen(info));
+        // Serial.print("set_notice");
+    }
+
+    static void unit_test()
+    {
+        // return;
+        // delay(10000);
+        // // set_notice("test");
+        // set_notice("连接成功");
+        while (true)
+        {
+            get_heart_rate();
+
+            delay(2000);
+            if (WearfitAlive)
+            {
+                // WearfitWrite.write("\xAB\x00\x04\xFF\xB1\x80\x01", 7);
+
+                // WearfitWrite.write("\xAB\x00\x04\xFF\x32\x80\x01", 7);
+
+                // set_notice("开始测量");
+
+                // {
+
+                //     static int count = 0, state = 0;
+                //     tm += 5;
+                //     switch (state)
+                //     {
+                //         case 0:
+                //             WearfitWrite.write("\xAB\x00\x04\xFF\x32\x80\x01", 7); // 启动测量
+                //             state = 1;
+                //             break;
+
+                //         case 1:
+                //             if (tm == 60)
+                //             {
+                //                 WearfitWrite.write("\xAB\x00\x04\xFF\x32\x80\x00", 7); // 停止测量
+                //                 state = 0;
+                //             }
+                //             break;
+
+                //     }
+                // }
+            }
+        }
+        
+        // WearfitWrite.write("\xAB\x00\x04\xFF\x31\x09\x01", 7);
     }
 
     void setup()
@@ -250,126 +351,21 @@ struct Wearfit
         Scheduler.startLoop(unit_test);
     }
 
-    static void notify_callback(BLEClientCharacteristic *chr, uint8_t *data, uint16_t len)
-    {
-        Serial.print("Optical data: ");
-        // Serial.printf("Optical len %d data: %.*s\n", len, len, data);
-        printHexList(data, len);
-
-        recv_forward(data, len);
-    }
-
-    static void recv_forward(uint8_t *buffer, uint8_t len)
-    {
-        if (buffer[4] == 0x31)
-        {
-            if (buffer[5] == 0x0A)
-            {
-                Serial.printf("H:%02X\n", buffer[6]); // 心率
-            }
-        }
-        if (buffer[4] == 0x91)
-        {
-            Serial.printf("B:%02X\n", buffer[7]); // 电量
-        }
-    }
-
-    static void get_heart_rate()
-    {
-        WearfitWrite.write("\xAB\x00\x04\xFF\x31\x0A\x01", 7);
-        Serial.write("get_heart_rate\n");
-    }
-
-    // static void get_battery()
-    // {
-    //     WearfitWrite.write("\xAB\x00\x03\xFF\xB2\x80", 6);
-    //     Serial.write("get_battery\n");
-    // }
-
-    static void set_ring_shake()
-    {
-        WearfitWrite.write("\xAB\x00\x04\xFF\xB1\x80\x01", 7);
-        Serial.write("set_ring_shake\n");
-    }
-
-    static void set_notice(char *info)
-    {
-        char buf[30] = "\xAB\x00\x05\xFF\x72\x80\x03\x02"; // 8
-        // WearfitWrite.write("\xAB\x00\x05\xFF\x72\x80\x09\x01", 8); // 0
-        sprintf(buf + 8, "%s", info);
-        buf[2] = buf[2] + strlen(info);
-        WearfitWrite.write(buf, 8 + strlen(info)); // 0
-        // printHexList((uint8_t *)buf, 8 + strlen(info));
-        Serial.write("set_notice\n");
-    }
-
-    static void unit_test()
-    {
-        delay(10000);
-        // set_notice("test");
-        set_notice("开始测量");
-        if (WearfitAlive)
-        {
-            // WearfitWrite.write("\xAB\x00\x04\xFF\xB1\x80\x01", 7);
-
-            // WearfitWrite.write("\xAB\x00\x04\xFF\x32\x80\x01", 7);
-
-            //set_notice("开始测量");
-
-            // {
-
-            //     static int count = 0, state = 0;
-            //     tm += 5;
-            //     switch (state)
-            //     {
-            //         case 0:
-            //             WearfitWrite.write("\xAB\x00\x04\xFF\x32\x80\x01", 7); // 启动测量
-            //             state = 1;
-            //             break;
-
-            //         case 1:
-            //             if (tm == 60)
-            //             {
-            //                 WearfitWrite.write("\xAB\x00\x04\xFF\x32\x80\x00", 7); // 停止测量
-            //                 state = 0;
-            //             }
-            //             break;
-
-            //     }
-            // }
-        }
-        // WearfitWrite.write("\xAB\x00\x04\xFF\x31\x09\x01", 7);
-    }
-
     void loop()
     {
         if (Serial.available())
         {
-            // Serial.write(Serial.read());
+            // Serial.print(Serial.read());
 
-            if ('T' == Serial.read())
-            {
-                WearfitWrite.write("\xAB\x00\x04\xFF\xB1\x80\x01", 7);
-            }
+            String tmp = Serial.readStringUntil('\n');
 
-            if ('B' == Serial.read())
-            {
-                WearfitWrite.write("\xAB\x00\x03\xFF\xB2\x80", 6);
-            }
+            // Serial.print(tmp);
 
-            if ('F' == Serial.read())
+            if ('S' == tmp.charAt(0))
             {
-                WearfitWrite.write("\xAB\x00\x03\xFF\x71\x80\x00", 7);
-            }
-
-            if ('S' == Serial.read())
-            {
-                WearfitWrite.write("\xAB\x00\x04\xFF\x31\x22\x01", 7);
-            }
-
-            if ('G' == Serial.read())
-            {
-                WearfitWrite.write("\xAB\x00\x04\xFF\x31\x22\x01", 7);
+                // Serial.print((char *)tmp.substring(1, tmp.length()).c_str());
+                // set_notice("连接成功");
+                set_notice((char *)tmp.substring(1, tmp.length()).c_str());
             }
         }
     }
